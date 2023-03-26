65b488aa485adea1020b98ec9c348969380fde1a
KoolTheba <gomez.teba@gmail.com>
2023-03-26 17:48:47 +0200

    feat: added deletePost feature

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query"
+import { useQuery, useMutation } from "react-query"
 
 async function fetchComments(id) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {method: 'GET'})
@@ -6,25 +6,24 @@ async function fetchComments(id) {
 }
 
 async function deletePost(id) {
-    const response = await fetch(`ttps://jsonplaceholder.typicode.com/postId/${id}`, {method: 'DELETE'})
-    return response.json()
-}
-
-async function updatePost(id){
-    const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${id}`, {method: 'PATCH', data: {title: 'Love React Query'}})
+    const response = await fetch(`https://jsonplaceholder.typicode.com/postId/${id}`, {method: 'DELETE'})
     return response.json()
 }
 
 export function PostDetail({ post }) {
-  const { data, isError, isFetching } = useQuery(['postComments', post.id], () => fetchComments(post.id))
+  const { data, isError, isLoading } = useQuery(['postComments', post.id], () => fetchComments(post.id))
+  const deleteMutation = useMutation(postId => deletePost(postId))
 
-  if(isFetching) return <p>Loading....</p>
+  if(isLoading) return <p>Loading....</p>
   if(isError) return <h3>Something went wrong, try again!</h3>
 
   return (
       <>
         <h3>{post.title}</h3>
-        <button>Delete</button> <button>Update title</button>
+        <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+        {deleteMutation.isLoading && <p>Deleting the post...</p>}
+        {deleteMutation.isError && <p style={{ color: 'red', fontSize: '14px'}}>Error deleting the post</p>}
+        {deleteMutation.isSuccess && <p style={{ color: 'green', fontSize: '14px' }}>Post has not been deleted due to the API :)</p>}
         <p>{post.body}</p>
         <h4>Comments</h4>
         <li key={data.id}>
